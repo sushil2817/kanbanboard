7231c948f230d9168441ba9d2e6f2970bef51344
sushil2817 <tony281723@gmail.com>
2025-08-09 09:52:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const todoCard = document.getElementById('todoCard');
+const inProgressCard = document.getElementById('inProgressCard');
+const inreviewCard = document.getElementById('inreviewCard');
+const acceptCard = document.getElementById('acceptCard');
+
+
+const allBoards = document.querySelectorAll('.board');
+
+allBoards.forEach(board =>{
+    board.addEventListener('dragover',()=>{
+        const flyingElement = document.querySelector('.flying')
+        console.log(board, 'Kuch toh mere upr se gaya',flyingElement);
+        if(flyingElement !=  null){
+            board.appendChild(flyingElement);
+        }
+    })
+})
